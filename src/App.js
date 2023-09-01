89f2436fe8ef3f105f4bc5b5f350b91777282da6
Pradyumancoder <shuklapradyuman786@gmail.com>
2023-09-01 23:41:39 +0530

    frontend-part-almost-done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import RegistrationForm from './Components/RegistrationForm/RegistrationForm';
+
+
+const App = () => {
+  // You can define state, hooks, and other logic here if needed
+
+  return (
+    <div className="App">
+      <h1>Registration App</h1>
+      <RegistrationForm />
+     
+    </div>
+  );
+};
+
+export default App;
